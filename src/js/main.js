e403cac359944d34ecbfecdc938e2fd14ea42cdb
Artem <artemlira84@gmail.com>
2024-03-08 12:43:09 +0200

    create app

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+import 'the-new-css-reset/css/reset.css';
+import '../styles/style.scss';
+// import burgerMenu from './burger-menu';
+// import tabs from "./tabs"; //Переключение табов
+// import './swiper'; // для подключения swiper
+import accordion from './accordion'; // для подключения accordion
+// import select from "./select"; //для подключения селекта
+// import "./phonemask"; //для подключения phonemask
+
+// burgerMenu(); // для бургер меню
+
+// ==================== Переключение табов ====================
+// tabs(".tabs__header", ".tabs__header-item", ".tabs__content-item", "active");
+
+// ==================== Переключение аккордиона ====================
+accordion();
+
+// ==================== Переключение селекта ====================
+// select(); // для селекта
